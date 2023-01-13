eeb6e284851b74f0904f6a5fd9a37910270d0976
unknown <yuvrajwadkar24@gmail.com>
2023-01-13 13:49:55 +0530

    message

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	
+	public static void main(String[] args) {
+	
+		List<String> list = new ArrayList<String>();
+		
+		list.add("ashutosh");
+		list.add("shubham");
+		list.add("arohi");
+		
+		System.out.println(list);
+	}
+
+}
